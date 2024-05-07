974cf1c602f6a5625d7363f41f3a85271727ce40
amaliehausmann <amaliehausmann@gmail.com>
2024-05-07 08:31:06 +0200

    node

---
+++ index.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+const port = 4000;
+
+http.createServer((Request, Response) => {
+    console.log('Server is running on http://localhost:${port}');
+    Response.writeHead (200, {'Content-Type': 'text/plain'});
+    Response.write('Hello World');
+    Response.end();
+}).listen(port)
