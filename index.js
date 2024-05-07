715aa247ce28e15082bc67c72efd5bc3e7a77e88
amaliehausmann <amaliehausmann@gmail.com>
2024-05-07 08:42:10 +0200

    rettelse

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http from 'http';
 const port = 4000;
 
 http.createServer((Request, Response) => {
-    console.log('Server is running on http://localhost:${port}');
+    console.log('Server is running on http://localhost:'+ port);
     Response.writeHead (200, {'Content-Type': 'text/plain'});
     Response.write('Hello World');
     Response.end();
