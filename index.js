2b0ab719598a19f7b23f10d2faf34cbfc89c093e
amaliehausmann <amaliehausmann@gmail.com>
2024-05-14 09:06:05 +0200

    dotenv

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-import http from 'http';
+import express from 'express'
+const app = express()
+const port = 3000;
 
-const port = 4000;
+import dotenv from 'dotenv';
 
-http.createServer((Request, Response) => {
-    console.log('Server is running on http://localhost:'+ port);
-    Response.writeHead (200, {'Content-Type': 'text/plain'});
-    Response.write('Hello World');
-    Response.end();
-}).listen(port)
+dotenv.config();
+
+const port = process.env.PORT_NUMBER;
+
+const api_key = process.env.SUPABASE_KEY;
+
+app.get('/', (request, response) => {
+response.send('Forside');
+})
+
+app.get('/About', (request, response) => {
+    response.send('Hvem er vi');
+})
+
+app.get('/Work', (request, response) => {
+    response.send('Hvad kan vi');
+})
+
+app.get('/Find', (request, response) => {
+    response.send('Find os');
+})
+
+app.get('/Productlist', (request, response) => {
+    response.send('Produkt liste');
+})
+
+app.get('/Productdetails', (request, response) => {
+    response.send('Produkt detaljer');
+})
+
+app.get('/Contact', (request, response) => {
+    response.send('Kontakt side');
+})
+
+// error handling
+app.get('*', (request, response) => {
+    response.status(404).send('404 - Siden blev ikke fundet');
+  });
+
+app.post('/', (request, response) => {
+    response.send('Endpoint til POST')
+    })
+
+app.listen(port, () => {
+    console.log('Webserver is running now on http://localhost:' + port);
+})
+
